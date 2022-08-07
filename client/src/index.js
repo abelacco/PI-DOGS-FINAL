7a41664d746e017670524a41931671d7d1134e20
abelacco <abel3121@gmail.com>
2022-08-07 09:51:49 -0500

    quitando el dotenv en el front

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import {Provider}  from 'react-redux';
 import store from "./store/index"
 import axios from "axios";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
