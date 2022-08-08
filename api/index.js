12e3e38b7a1abf2a5bd88d8a0c0adafb70bb920a
abelacco <abel3121@gmail.com>
2022-08-07 21:13:48 -0500

    first commit

--- api/index.js
+++ api/index.js
@@ -34,6 +34,6 @@ const getTemps = getTemperaments();
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log("%s listening at: 3001"); // eslint-disable-line no-console
   });
 });
